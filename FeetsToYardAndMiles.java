82fc1b8c9e2fdcb7b27bde329678639cdaf0fdbe
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-03-01 00:20:08 +0530

    [Nikhil] Add: the find the distance in yards and miles

---
+++ FeetsToYardAndMiles.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class FeetsToYardAndMiles {
+    public static void main(String[] args) {
+        
+	Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the distance in feet: ");
+        double feet = sc.nextDouble();
+        double yards = feet / 3.0;
+        double miles = yards / 1760.0;
+        System.out.println("The distance in yards is " + yards + " and in miles is " + miles + " for the given " + feet + " feet.");
+        
+    }
+}
